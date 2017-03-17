0069b3611ab20c6c8aaa923ec00947d163fbcdd0
Martín Peveri <martinpeveri@gmail.com>
2017-03-16 23:28:29 -0300

    Api send_sms

--- apps/modem.py
+++ apps/modem.py
@@ -8,10 +8,17 @@ class ModemGSM(object):
     """
     Class for manage GSM Modem
     """
+    instance = None
     port = None
 
-    def __init__(self, serial):
-        self.port = serial.Serial(serial)
+    def __init__(self):
+        self.port = serial.Serial(app.config["SERIAL_GSM"])
+
+    # Singleton
+    def __new__(cls, *args, **kargs):
+        if cls.instance is None:
+            cls.instance = object.__new__(cls, *args, **kargs)
+        return cls.instance
 
     def send_sms(self, number, message):
         time.sleep(1)
--- apps/sms/api.py
+++ apps/sms/api.py
@@ -1,14 +1,39 @@
+from flask import request
 from flask_restful import Resource
 
 from app import api
+from apps.modem import ModemGSM
 from apps.sms import models
-from apps.utils import MiximJson
 
 
-class GetUsers(Resource, MiximJson):
-    def get(self):
-        users = models.User.query.all()
-        return self.to_json(users)
+class SendSms(Resource):
+    """
+    Send sms API with ModemGSM class
+    """
+    def post(self):
+        # Parameters sms
+        number = request.form['number']
+        message = request.form['message']
+
+        # Check parameters
+        if number and message:
+            try:
+                # Instance GSM Class
+                instanceGsm = ModemGSM()
+                # Send SMS
+                instanceGsm.send_sms(number, message)
+            except Exception:
+                return {
+                    'result': 'error',
+                    'description': 'Error endpoint send_sms'
+                }
+
+            return {'result': 'ok', description: 'Success'}
+        else:
+            return {
+                'result': 'error',
+                'description': 'The number and message parameters are needed'
+            }
 
 
-api.add_resource(GetUsers, '/api/getusers')
+api.add_resource(SendSms, '/api/send_sms')
